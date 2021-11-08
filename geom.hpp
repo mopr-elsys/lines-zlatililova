bac47dad39025a05d495499c0a056b22ba53d286
Zlatina Lilova <48581738+zlatililova@users.noreply.github.com>
2021-11-08 08:36:58 +0200

    Update geom.hpp

--- geom.hpp
+++ geom.hpp
@@ -34,7 +34,7 @@ struct Line {
     Line(double A = 0, double B = 0, double C = 0) : A(A), B(B), C(C) {}
 
     Line(const Point& p1, const Point& p2) {
-        A = p2.y - p1.y; B = p1.x - p2.x; C = -(A.p + B.q);
+        A = p2.y - p1.y; B = p1.x - p2.x; C = -(A*p1.x + B.p1.y);
     }
 
     bool parallel(const Line& other) const {
